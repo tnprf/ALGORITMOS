93f76b9216fb84eb7e968ea67a3e653f035308e3
tnprf <tauanitalo10@gmail.com>
2025-03-13 21:36:51 -0300

    Update and rename conta 01.by to 01.py

---
+++ 01.py
@@ -0,0 +1,4 @@
+#problema: escreva um algoritmo para ler um valor numerico (do teclado) e escrever (na tela) o seu antecessor
+print("qual numero voce deseja?")
+numero= int(input())
+print(numero-1)

ec3a2b822bab9820cb2f3134a88f098eba6a4073
tnprf <tauanitalo10@gmail.com>
2025-03-13 21:49:03 -0300

    Create 02.py

---
+++ 02.py
@@ -0,0 +1,6 @@
+#escreva um algoritmo para ler dimensoes de um retangulo (base e altura), calcular e escrever a area do retangulo
+print("quais sao as dimensoes de base?")
+base= int(input())
+print("quais as dimensoes de altura?")
+altura= int(input())
+print(10*2)
